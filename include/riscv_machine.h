02de6a9f881820a3c67c862f692caccb4b53d045
Mark Wyse <muwyse@gmail.com>
2022-03-17 10:04:11 -0700

    fix param rom offsets for CC_X and CC_Y

--- include/riscv_machine.h
+++ include/riscv_machine.h
@@ -127,10 +127,11 @@ struct RISCVMachine {
 #define CLINT_BASE_ADDR 0x00300000
 #define CLINT_SIZE      0x000c0000
 
+// These must be kept up to date with the RTL code and SDK aviary.h
 #define PARAM_ROM_BASE_ADDR 0x20000
-#define PARAM_ROM_SIZE 0x0124
-#define PARAM_CC_X_DIM 0x0004
-#define PARAM_CC_Y_DIM 0x0008
+#define PARAM_ROM_SIZE 0x012C
+#define PARAM_CC_X_DIM 0x0000
+#define PARAM_CC_Y_DIM 0x0004
 
 #define HOST_BASE_ADDR 0x00100000
 #define HOST_SIZE      0x00100000
